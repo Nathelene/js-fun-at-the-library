ee5439a2fdc3e45c98eb3f2fb1badf45d80d9596
Natalie Baker <nathelene76@gmail.com>
2023-03-24 10:22:09 -0600

    add function

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,27 @@
 
-module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+function shelfBook(book,shelf){
+ 
+  if (shelf.length < 3){
+   return shelf.unshift(book)
+  }
 };
+
+function unshelfBook(title,shelf) {
+
+  for (var i=0; i < shelf.length; i++){
+  if (shelf[i].title === title){
+    return shelf.splice(i, 1)
+   }
+ }
+}
+
+function listTitles(){
+  
+}
+
+module.exports = {
+   shelfBook,
+   unshelfBook,
+   listTitles,
+  // searchShelf
+}
